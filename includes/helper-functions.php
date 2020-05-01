65ff401ebf78900fa8e87b4f9dee93e2c6fc800d
Vasilis Koutsopoulos <vaskou@hotmail.com>
2020-05-01 22:13:04 +0300

    -WIP

---
+++ includes/helper-functions.php
@@ -0,0 +1,29 @@
+<?php
+
+defined( 'ABSPATH' ) or die();
+
+function currency_exchange_rate_get_latest( $base = 'USD' ) {
+	return Currency_Exchange_Rates::getInstance()->get_latest( $base );
+}
+
+function currency_exchange_rates_convert( $value, $to, $from = 'USD' ) {
+
+	$transient_name = 'currency_exchange_rates_latest_' . $from;
+
+	$currencies = get_transient( $transient_name );
+
+	if ( empty( $currencies ) ) {
+		$currencies = Currency_Exchange_Rates::getInstance()->get_latest( $from );
+	}
+
+	if ( ! is_wp_error( $currencies ) && ! empty( $currencies['rates'][ $to ] ) ) {
+		set_transient( $transient_name, $currencies, DAY_IN_SECONDS );
+
+		$rate = $currencies['rates'][ $to ];
+
+		return number_format_i18n( $value * $rate, 0 );
+	} else {
+		return false;
+	}
+
+}
